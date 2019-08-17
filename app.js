2ec51c1c0dd86c2d852a8cff47b40b8eba64cc0c
Amruth <amruthk99@gmail.com>
2019-08-17 18:07:33 +0530

    added routes

--- app.js
+++ app.js
@@ -3,14 +3,35 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
-app.use(express.static('public'));
+app.use(express.static('views'));
+app.set("view engine","ejs")
 
-
-app.get('/',(request,response)=>{
-    response.sendFile(__dirname + '/public/student_login.html');
+    app.get('/',(request,response)=>{
+    response.render('student_login');
    });
+    app.get('/home',(request,response)=>{
+    response.render('home.ejs');
+   });
+   app.get('/Academics',(request,response)=>{
+       let studentInfo = [
+           {name:"Hitesh Atluri", Roll:"ECE17U004",image:"hitesh.jpeg",branch:"Electronics and Communication Engineering (ECE)",semester:"5th",currentYear:"3rd",batch:"2017-2021"},
+           {name:"Amruth Kuntamalla", Roll:"ECE17U003",image:"amruth.jpeg",branch:"Electronics and Communication Engineering (ECE)",semester:"5th",currentYear:"3rd",batch:"2017-2021"},
+           {name:"Satyam Pavan", Roll:"CSE17U017",image:"pavan.jpeg",branch:"Computer Science Engineering (CSE)",semester:"5th",currentYear:"3rd",batch:"2017-2021"}
+        ];
+    response.render( 'Academic_Summary',{student:studentInfo});
+   });
+   app.get('/attendance',(request,response)=>{
+    response.render('attendance.ejs');
+   });
+   app.get('/mess',(request,response)=>{
+    response.render('mess');
+   });
+   app.get('/profile',(request,response)=>{
+    response.render('profile');
+   });
+
 
 //add the router
 app.use('/', router);
-app.listen(process.env.port || 3000);
-console.log('Running at Port 3000');
+app.listen(process.env.port || 4000);
+console.log('Running at Port 4000');
